53d39c8a0a9cf2de68409cc22624d8ee2a12662a
Jesus Rodriguez <foxandxss@gmail.com>
2025-06-27 21:37:49 +0200

    initial idea

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  i18n: {
+    locales: ["es", "en"], // Locales you want to support
+    defaultLocale: "es", // Default locale (fallback)
+
+    routing: {
+      prefixDefaultLocale: true, // Ensures that your default locale is prefixed aswell
+    },
+  },
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
---
+++ src/i18n/ui.ts
@@ -0,0 +1,18 @@
+export const languages = {
+  en: 'English',
+  es: 'Spanish',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+    'nav.hello': 'Hello',
+    'nav.twitter': 'Twitter',
+  },
+  es: {
+    'nav.home': 'Inicio',
+    'nav.hello': 'Hola',
+  },
+} as const;
